7b7cdf9a962a82741e310a84a281d5070c73e158
D3ndr0k <102122033+D3ndr0k@users.noreply.github.com>
2022-10-20 19:50:09 -0300

    Add files via upload

---
+++ controlproductos.php
@@ -0,0 +1,32 @@
+<?php
+
+include "config.php";
+session_start();
+
+if(!empty($_POST["submit"])){
+  if (empty($_POST["codigo"]) or empty($_POST["nombre"]) or empty($_POST["descripcion"]) or empty($_POST["fechavencimiento"])or empty($_POST["precio"])or empty($_POST["marca"])){
+    echo "<div class='alerta'> Completa todos los campos </div>";
+  }else{
+
+    $imagen = addslashes(file_get_contents($_FILES['imagen']['tmp_name']));
+    $codigo = $_POST["codigo"];
+    $nombre = $_POST["nombre"];
+    $descripcion = $_POST["descripcion"];
+    $fechavencimiento = $_POST["fechavencimiento"];
+    $precio = $_POST["precio"];
+    $marca = $_POST["marca"];
+
+    
+    $sql=$conn->query("insert into productos(codigo,nombre,descripcion,fechavencimiento,precio,marca,imagen) values('$codigo','$nombre','$descripcion','$fechavencimiento','$precio','$marca','$imagen')");
+   if ($sql==1) {
+    echo 'registrado';
+   }else{
+     echo 'error';
+   }
+ 
+  }
+
+  }
+
+
+?>
