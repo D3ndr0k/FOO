9adee5a2457f4b8fa655de90146d4d242d53330b
D3ndr0k <102122033+D3ndr0k@users.noreply.github.com>
2022-10-20 19:49:28 -0300

    Add files via upload

---
+++ agregarpublicacion.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agregar publicación</title>
+    <link rel="stylesheet" type="text/css" href="./assets/css/style_locales.css">
+</head>
+
+<body bgcolor="#ffe8ef">
+    <br><center>
+    <h2 style="color:#7EFF30;">Ingresar información del producto</h2><br><br><br><br>
+    <div class="centro"> 
+    <center><form action="" method="POST" enctype="multipart/form-data">
+
+    <h3> <p>Imagen del producto</p><h3> 
+        <input type="file" name="imagen" >
+        <input type="number" placeholder=" código del producto" name="codigo">
+        <input type="text" placeholder=" nombre del producto" name="nombre">
+        <input type="text" placeholder=" descripción" name="descripcion">
+        <p>Fecha de vencimiento</p>
+        <input type="date" name="fechavencimiento">
+        <input type="number" step="0.01" min="0" placeholder=" precio" name="precio">
+        <input type="text" placeholder=" marca" name="marca">
+       <center> <input class="boton"type="submit" value="ingresar" name="submit" >
+       <?php 
+    include ("config.php");
+    include ("controlproductos.php");
+    
+    ?>
+    </form>
+    </div>
+</body>
+</html>
